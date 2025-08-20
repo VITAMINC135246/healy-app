79475811b08577b0c715d2614fa3898c551dad6a
Zhengtao0719 <victor20050719@gmail.com>
2025-08-20 19:21:33 +0800

    chore: init Expo + TypeScript project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
+// App.tsx
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>kapa is dog ✅</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
 });
